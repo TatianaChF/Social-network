8c0532721a2b7f69e998b679b3acca233c16323d
TatyanaChekina <tatian.chefox@gmail.com>
2022-10-26 15:19:44 +0300

    adding a default value and assigning it to the state

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,24 @@
 const ADD_MESSAGE = 'ADD-MESSAGE';
 const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+    dialogsData: [
+        { id: 1, name: "Alexander" },
+        { id: 2, name: "Anna" },
+        { id: 3, name: "Irina" },
+        { id: 4, name: "Tatiana" },
+        { id: 5, name: "Dmitry" },
+        { id: 6, name: "Viktor" }
+    ],
+    messagesData: [
+        { id: 1, message: "Hi" },
+        { id: 2, message: "How are you?" },
+        { id: 3, message: "Yo" }
+    ],
+    newMessageText: "",
+}
+
+const dialogsReducer = (state = initialState, action) => {
 
     switch (action.type) {
         case ADD_MESSAGE:
