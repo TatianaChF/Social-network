4fd0c8362641a9eaf7a82acde6854b361244b0ac
TatyanaChekina <tatian.chefox@gmail.com>
2022-10-26 13:25:40 +0300

    moving logic for reducer from dispatch

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,4 +1,18 @@
 const profileReducer = (state, action) => {
 
+    if(action.type === ADD_POST) {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    } else if(action.type === UPDATE_NEW_POST_TEXT) {
+        this._state.profilePage.newPostText = action.newPost;
+        this._callSubscriber(this._state);
+    }
+
     return state;
 }
