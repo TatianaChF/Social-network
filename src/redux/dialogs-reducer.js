9c6908db50a9caea8fd5898be2e6cc5dd3b5d3d9
TatyanaChekina <tatian.chefox@gmail.com>
2022-10-26 13:28:16 +0300

    adding logic and constants to the reducer for dialogs

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,4 +1,20 @@
+const ADD_MESSAGE = 'ADD-MESSAGE';
+const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
+
 const dialogsReducer = (state, action) => {
+
+    if(action.type === ADD_MESSAGE) {
+        let newMessage = {
+            id: 4,
+            message: this._state.dialogsPage.newMessageText
+        };
+        this._state.dialogsPage.messagesData.push(newMessage);
+        this._state.dialogsPage.newMessageText = '';
+        this._callSubscriber(this._state);
+    } else if(action.type === UPDATE_NEW_MESSAGE_TEXT) {
+        this._state.dialogsPage.newMessageText = action.newMessage;
+        this._callSubscriber(this._state);
+    }
 
     return state;
 }
