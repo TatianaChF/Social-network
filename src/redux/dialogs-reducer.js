89df6eeb6e16010c943768de3edb3b7f17c52e52
TatyanaChekina <tatian.chefox@gmail.com>
2022-10-26 14:00:47 +0300

    change if to switch

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -3,18 +3,20 @@ const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 
 const dialogsReducer = (state, action) => {
 
-    if(action.type === ADD_MESSAGE) {
-        let newMessage = {
-            id: 4,
-            message: state.newMessageText
-        };
-        state.messagesData.push(newMessage);
-        state.newMessageText = '';
-    } else if(action.type === UPDATE_NEW_MESSAGE_TEXT) {
-        state.newMessageText = action.newMessage;
+    switch (action.type) {
+        case ADD_MESSAGE:
+            let newMessage = {
+                id: 4,
+                message: state.newMessageText
+            };
+            state.messagesData.push(newMessage);
+            state.newMessageText = '';
+            return state;
+        case UPDATE_NEW_MESSAGE_TEXT:
+            state.newMessageText = action.newMessage;
+            return state;
     }
 
-    return state;
 }
 
 export default dialogsReducer;
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -3,19 +3,21 @@ const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 const profileReducer = (state, action) => {
 
-    if(action.type === ADD_POST) {
-        let newPost = {
-            id: 5,
-            message: state.newPostText,
-            likesCount: 0
-        };
-        state.posts.push(newPost);
-        state.newPostText = '';
-    } else if(action.type === UPDATE_NEW_POST_TEXT) {
-        state.newPostText = action.newPost;
+    switch (action.type) {
+        case ADD_POST:
+            let newPost = {
+                id: 5,
+                message: state.newPostText,
+                likesCount: 0
+            };
+            state.posts.push(newPost);
+            state.newPostText = '';
+            return state;
+        case UPDATE_NEW_POST_TEXT:
+            state.newPostText = action.newPost;
+            return state;
     }
 
-    return state;
 }
 
 export default profileReducer;
