0a0994de14820b27b49d6515046d777370be67e5
TatyanaChekina <tatian.chefox@gmail.com>
2022-10-27 21:25:15 +0300

    rendering a container component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,12 +1,13 @@
 import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
+import MyPostsContainer from "./MyPosts/Post/MyPostsContainer";
 
 const Profile = (props) => {
 
     return (
         <div>
             <ProfileInfo />
-            <MyPosts posts={props.data.posts}
+            <MyPostsContainer posts={props.data.posts}
                      newPost={props.data.newPostText}
                      dispatch={props.dispatch} />
         </div>
