2ee581d5da9dd9f81c7cd492ffbe813cf10cd07f
TatyanaChekina <tatian.chefox@gmail.com>
2022-10-30 19:09:54 +0300

    moving a container component

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../../redux/profile-reducer";
-import MyPosts from "../MyPosts";
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profile-reducer";
+import MyPosts from "./MyPosts";
 
 const MyPostsContainer = (props) => {
 
