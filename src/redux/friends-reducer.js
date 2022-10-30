32152b8f4722dfb9b9b971b0d7bd62db95c1ee02
TatyanaChekina <tatian.chefox@gmail.com>
2022-10-30 13:14:03 +0300

    adding default values for friends page

--- src/redux/friends-reducer.js
+++ src/redux/friends-reducer.js
@@ -1,5 +1,11 @@
 let initialState = {
-
+    friendsPage: [
+        { id: 1, name: "Alexander", avatar: "https://i.insider.com/61d1c0e2aa741500193b2d18?width=700" },
+        { id: 2, name: "Tatiana", avatar: "https://cdn.mos.cms.futurecdn.net/fiDmrNrJsmGD2t57x4dRdG-1200-80.jpg" },
+        { id: 3, name: "Anna", avatar: "https://www.farmersalmanac.com/wp-content/uploads/2021/04/forget-me-not-flower-as309740666.jpeg" },
+        { id: 4, name: "Viktor", avatar: "https://rrsg.s3.amazonaws.com/wp-content/uploads/2018/12/Veyron-Grand-Sport-Vitesse-Black-Bess-1024x562.jpg" },
+        { id: 5, name: "Irina", avatar: "https://www.gannett-cdn.com/presto/2020/03/09/PDTF/1ad7be1a-1b59-438e-9627-700753bc9867-GettyImages-1132562466.jpg" }
+    ]
 };
 
 const friendsReducer = (state = initialState, action) => {
