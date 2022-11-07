90d69bba99391b04ac16b4babb8ee4f8c22673ab
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-07 21:35:26 +0300

    create a copy of the state

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -18,6 +18,7 @@ const profileReducer = (state = initialState, action) => {
                 message: state.newPostText,
                 likesCount: 0
             };
+            let stateCopy = {...state};
             state.posts.push(newPost);
             state.newPostText = '';
             return state;
