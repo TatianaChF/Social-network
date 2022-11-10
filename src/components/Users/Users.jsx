a355ffd7ce1bfd0fd774785018fef67029c8cf27
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-10 21:04:50 +0300

    bringing users to the page

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,6 +1,29 @@
 const Users = (props) => {
     return (
-        <div>USERS WILL BE HERE</div>
+        <div>
+            {
+                props.users.map( user => <div key={user.id}>
+                    <span>
+                        <div>
+                            <img src={user.photoUrl} />
+                        </div>
+                        <div>
+                            <button>Follow</button>
+                        </div>
+                    </span>
+                    <span>
+                        <span>
+                            <div>{user.fullName}</div>
+                            <div>{user.status}</div>
+                        </span>
+                        <span>
+                            <div>{user.location.city}</div>
+                            <div>{user.location.country}</div>
+                        </span>
+                    </span>
+                </div> )
+            }
+        </div>
     )
 }
 
