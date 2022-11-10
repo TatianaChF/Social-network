11c5e43115fd7c06ad0ebd18ed230754554641b5
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-10 19:06:32 +0300

    adding individual id

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -2,11 +2,11 @@ let initialState = {
     users: [
         { id: 1, fullName: 'Aleksander', status: 'I like cats!',
             location: { city: 'St. Petersburg', country: 'Russia' } },
-        { id: 1, fullName: 'Tatiana', status: 'I like cats!',
+        { id: 2, fullName: 'Tatiana', status: 'I like cats!',
             location: { city: 'I am a musician', country: 'Russia' } },
-        { id: 1, fullName: 'Irina', status: 'All the best!',
+        { id: 3, fullName: 'Irina', status: 'All the best!',
             location: { city: 'London', country: 'England' } },
-        { id: 1, fullName: 'Anna', status: 'I watch series',
+        { id: 4, fullName: 'Anna', status: 'I watch series',
             location: { city: 'Berlin', country: 'Germany' } },
     ]
 }
