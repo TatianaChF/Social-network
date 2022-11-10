6e3ce33eba135a2c8cae94dc240fa148e890c57d
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-10 20:59:42 +0300

    removed comments

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -4,14 +4,14 @@ const SET_USER = 'SET-USER';
 
 let initialState = {
     users: [
-        // { id: 1, followed: true, fullName: 'Aleksander', status: 'I like cats!',
-        //     location: { city: 'St. Petersburg', country: 'Russia' } },
-        // { id: 2, followed: true, fullName: 'Tatiana', status: 'I like cats!',
-        //     location: { city: 'I am a musician', country: 'Russia' } },
-        // { id: 3, followed: false, fullName: 'Irina', status: 'All the best!',
-        //     location: { city: 'London', country: 'England' } },
-        // { id: 4, followed: false, fullName: 'Anna', status: 'I watch series',
-        //     location: { city: 'Berlin', country: 'Germany' } }
+        { id: 1, followed: true, fullName: 'Aleksander', status: 'I like cats!',
+            location: { city: 'St. Petersburg', country: 'Russia' } },
+        { id: 2, followed: true, fullName: 'Tatiana', status: 'I like cats!',
+            location: { city: 'I am a musician', country: 'Russia' } },
+        { id: 3, followed: false, fullName: 'Irina', status: 'All the best!',
+            location: { city: 'London', country: 'England' } },
+        { id: 4, followed: false, fullName: 'Anna', status: 'I watch series',
+            location: { city: 'Berlin', country: 'Germany' } }
     ]
 }
 
