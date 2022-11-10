0c80ea9026d04ee6d9e3171456e91a4518c999b4
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-10 19:03:19 +0300

    creating an array of users and assigning the default state to the state that the reducer receives

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,4 +1,17 @@
-const usersReducer = (state, action) => {
+let initialState = {
+    users: [
+        { id: 1, fullName: 'Aleksander', status: 'I like cats!',
+            location: { city: 'St. Petersburg', country: 'Russia' } },
+        { id: 1, fullName: 'Tatiana', status: 'I like cats!',
+            location: { city: 'St. Petersburg', country: 'Russia' } },
+        { id: 1, fullName: 'Irina', status: 'I like cats!',
+            location: { city: 'London', country: 'England' } },
+        { id: 1, fullName: 'Anna', status: 'I like cats!',
+            location: { city: 'Berlin', country: 'Germany' } },
+    ]
+}
+
+const usersReducer = (state = initialState, action) => {
 
 }
 
