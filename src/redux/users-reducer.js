a0e9f832dd10e802a4c0098a33ff47da3b27a517
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-10 21:02:44 +0300

    adding an avatar to users

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -4,13 +4,17 @@ const SET_USER = 'SET-USER';
 
 let initialState = {
     users: [
-        { id: 1, followed: true, fullName: 'Aleksander', status: 'I like cats!',
+        { id: 1, photoUrl: 'https://i.insider.com/61d1c0e2aa741500193b2d18?width=700', followed: true,
+            fullName: 'Aleksander', status: 'I like cats!',
             location: { city: 'St. Petersburg', country: 'Russia' } },
-        { id: 2, followed: true, fullName: 'Tatiana', status: 'I like cats!',
+        { id: 2, photoUrl: 'https://cdn.mos.cms.futurecdn.net/fiDmrNrJsmGD2t57x4dRdG-1200-80.jpg', followed: true,
+            fullName: 'Tatiana', status: 'I like cats!',
             location: { city: 'I am a musician', country: 'Russia' } },
-        { id: 3, followed: false, fullName: 'Irina', status: 'All the best!',
+        { id: 3, photoUrl: 'https://www.gannett-cdn.com/presto/2020/03/09/PDTF/1ad7be1a-1b59-438e-9627-700753bc9867-GettyImages-1132562466.jpg',
+            followed: false, fullName: 'Irina', status: 'All the best!',
             location: { city: 'London', country: 'England' } },
-        { id: 4, followed: false, fullName: 'Anna', status: 'I watch series',
+        { id: 4, photoUrl: 'https://www.farmersalmanac.com/wp-content/uploads/2021/04/forget-me-not-flower-as309740666.jpeg',
+            followed: false, fullName: 'Anna', status: 'I watch series',
             location: { city: 'Berlin', country: 'Germany' } }
     ]
 }
