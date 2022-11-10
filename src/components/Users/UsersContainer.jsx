63dc71be637e6791dd4c8a97f6c15e035e4a76d5
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-10 20:30:35 +0300

    creating the structure of the container component

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,14 @@
-const UsersContainer = (props) => {
-    return (
-        <div></div>
-    )
+import {connect} from "react-redux";
+import Users from "./Users";
+
+const mapStateToPropsUsers = () => {
+
 }
+
+const mapDispatchToPropsUsers = () => {
+
+}
+
+const UsersContainer = connect(mapStateToPropsUsers, mapDispatchToPropsUsers)(Users);
 
 export default UsersContainer;
