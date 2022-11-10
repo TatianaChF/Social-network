55d86bf2a3152ef66c51558cb2ffb7c2ea50f74b
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-10 19:11:24 +0300

    adding add friend property

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,12 +1,12 @@
 let initialState = {
     users: [
-        { id: 1, fullName: 'Aleksander', status: 'I like cats!',
+        { id: 1, followed: true, fullName: 'Aleksander', status: 'I like cats!',
             location: { city: 'St. Petersburg', country: 'Russia' } },
-        { id: 2, fullName: 'Tatiana', status: 'I like cats!',
+        { id: 2, followed: true, fullName: 'Tatiana', status: 'I like cats!',
             location: { city: 'I am a musician', country: 'Russia' } },
-        { id: 3, fullName: 'Irina', status: 'All the best!',
+        { id: 3, followed: false, fullName: 'Irina', status: 'All the best!',
             location: { city: 'London', country: 'England' } },
-        { id: 4, fullName: 'Anna', status: 'I watch series',
+        { id: 4, followed: false, fullName: 'Anna', status: 'I watch series',
             location: { city: 'Berlin', country: 'Germany' } },
     ]
 }
