1bc0968cc4372886cca79a54a8b71aa9e7f1ad9d
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-10 19:05:36 +0300

    adding individual statuses

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,10 +3,10 @@ let initialState = {
         { id: 1, fullName: 'Aleksander', status: 'I like cats!',
             location: { city: 'St. Petersburg', country: 'Russia' } },
         { id: 1, fullName: 'Tatiana', status: 'I like cats!',
-            location: { city: 'St. Petersburg', country: 'Russia' } },
-        { id: 1, fullName: 'Irina', status: 'I like cats!',
+            location: { city: 'I am a musician', country: 'Russia' } },
+        { id: 1, fullName: 'Irina', status: 'All the best!',
             location: { city: 'London', country: 'England' } },
-        { id: 1, fullName: 'Anna', status: 'I like cats!',
+        { id: 1, fullName: 'Anna', status: 'I watch series',
             location: { city: 'Berlin', country: 'Germany' } },
     ]
 }
