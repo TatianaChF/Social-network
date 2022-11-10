1fc6f929db36a8c1a763e8f3178c3231a80bc811
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-10 20:26:20 +0300

    creating a container component and adding it to the router

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {Route, Routes} from "react-router-dom";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import Friends from "./components/Friends/Friends";
-import Users from "./components/Users/Users";
+import UsersContainer from "./components/Users/UsersContainer";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
 const App = (props) => {
@@ -26,7 +26,7 @@ const App = (props) => {
                       <Route path="/music" element={<Music />} />
                       <Route path="/settings" element={<Settings />} />
                       <Route path="/friends" element={<Friends />} />
-                      <Route path="/users" element={<Users />} />
+                      <Route path="/users" element={<UsersContainer />} />
                   </Routes>
               </div>
           </div>
---
+++ src/components/Users/UsersContainer.jsx
@@ -0,0 +1,7 @@
+const UsersContainer = (props) => {
+    return (
+        <div></div>
+    )
+}
+
+export default UsersContainer;
