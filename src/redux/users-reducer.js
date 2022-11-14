e1d83c3e11fc937608d74178ce0bae7613cfa571
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-14 14:44:28 +0300

    deleting array of users and passing users through setUserAC

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,20 +3,7 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USER = 'SET-USER';
 
 let initialState = {
-    users: [
-        { id: 1, photoUrl: 'https://i.insider.com/61d1c0e2aa741500193b2d18?width=700', followed: true,
-            fullName: 'Aleksander', status: 'I like cats!',
-            location: { city: 'St. Petersburg', country: 'Russia' } },
-        { id: 2, photoUrl: 'https://cdn.mos.cms.futurecdn.net/fiDmrNrJsmGD2t57x4dRdG-1200-80.jpg', followed: true,
-            fullName: 'Tatiana', status: 'I like cats!',
-            location: { city: 'I am a musician', country: 'Russia' } },
-        { id: 3, photoUrl: 'https://www.gannett-cdn.com/presto/2020/03/09/PDTF/1ad7be1a-1b59-438e-9627-700753bc9867-GettyImages-1132562466.jpg',
-            followed: false, fullName: 'Irina', status: 'All the best!',
-            location: { city: 'London', country: 'England' } },
-        { id: 4, photoUrl: 'https://www.farmersalmanac.com/wp-content/uploads/2021/04/forget-me-not-flower-as309740666.jpeg',
-            followed: false, fullName: 'Anna', status: 'I watch series',
-            location: { city: 'Berlin', country: 'Germany' } }
-    ]
+    users: [ ]
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -43,7 +30,7 @@ const usersReducer = (state = initialState, action) => {
                 } )
             }
         case SET_USER:
-            return {...state, users: [ ...state.users, action.users ] }
+            return {...state, users: [ ...state.users, ...action.users ] }
         default:
             return state;
     }
@@ -52,6 +39,6 @@ const usersReducer = (state = initialState, action) => {
 
 export const followAC = (userId) => ({ type: FOLLOW, userId })
 export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId })
-export const setUserAC = (user) => ({type: SET_USER, user})
+export const setUserAC = (users) => ({type: SET_USER, users})
 
 export default usersReducer;
