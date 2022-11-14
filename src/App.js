48343de68322514f3d54584287d8fad5e2f6731f
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-14 13:49:48 +0300

    Removed debugger

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import UsersContainer from "./components/Users/UsersContainer";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
 const App = (props) => {
-    debugger;
+
   return (
           <div className="app-wrapper">
               <Header />
