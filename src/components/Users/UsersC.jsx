49eeae4ec678cc7fd303b65bdc16e00e8d94fecb
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-24 20:52:53 +0300

    transferring code from a functional component to a class component and adding this

--- src/components/Users/UsersC.jsx
+++ src/components/Users/UsersC.jsx
@@ -5,6 +5,47 @@ import userPhoto from "../../assets/images/defaultUser.jpg";
 
 class Users extends React.Component {
 
+    getUsers = () => {
+        if(this.props.users.length === 0) {
+
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then( response => {
+                this.props.setUsers(response.data.items);
+            });
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.getUsers}>GET USERS</button>
+                {
+                    this.props.users.map( user => <div key={user.id}>
+                    <span>
+                        <div>
+                            <img src={ user.photos.small != null ? user.photos.small : userPhoto } className={styles.userPhoto} />
+                        </div>
+                        <div>
+                            { user.followed
+                                ? <button onClick={() => { this.props.unfollow(user.id) }}>Unfollow</button>
+                                : <button onClick={() => { this.props.follow(user.id) }}>Follow</button> }
+                        </div>
+                    </span>
+                        <span>
+                        <span>
+                            <div>{user.name}</div>
+                            <div>{user.status}</div>
+                        </span>
+                        <span>
+                            <div>Moscow</div>
+                            <div>Russia</div>
+                        </span>
+                    </span>
+                    </div> )
+                }
+            </div>
+        )
+    }
 }
 
 export default Users;
