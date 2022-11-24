a10bac148e56a235e7fdda85a172589c3627de11
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-24 20:44:31 +0300

    creating a class component

---
+++ src/components/Users/UsersC.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Users.module.css";
+import axios from "axios";
+import userPhoto from "../../assets/images/defaultUser.jpg";
+
+class Users extends React.Component {
+
+}
+
+export default Users;
