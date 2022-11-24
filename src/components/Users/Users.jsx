96e235ea052d20dd74f2657f4929f5490fe6b715
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-24 20:59:35 +0300

    deleting a functional component and renaming a class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,35 +1,37 @@
+import React from "react";
 import styles from "./Users.module.css";
 import axios from "axios";
 import userPhoto from "../../assets/images/defaultUser.jpg";
 
-const Users = (props) => {
+class Users extends React.Component {
 
-    let getUsers = () => {
-        if(props.users.length === 0) {
+    getUsers = () => {
+        if(this.props.users.length === 0) {
 
             axios.get("https://social-network.samuraijs.com/api/1.0/users").then( response => {
-                props.setUsers(response.data.items);
+                this.props.setUsers(response.data.items);
             });
 
         }
     }
 
-    return (
-        <div>
-            <button onClick={getUsers}>GET USERS</button>
-            {
-                props.users.map( user => <div key={user.id}>
+    render() {
+        return (
+            <div>
+                <button onClick={this.getUsers}>GET USERS</button>
+                {
+                    this.props.users.map( user => <div key={user.id}>
                     <span>
                         <div>
                             <img src={ user.photos.small != null ? user.photos.small : userPhoto } className={styles.userPhoto} />
                         </div>
                         <div>
                             { user.followed
-                                ? <button onClick={() => { props.unfollow(user.id) }}>Unfollow</button>
-                                : <button onClick={() => { props.follow(user.id) }}>Follow</button> }
+                                ? <button onClick={() => { this.props.unfollow(user.id) }}>Unfollow</button>
+                                : <button onClick={() => { this.props.follow(user.id) }}>Follow</button> }
                         </div>
                     </span>
-                    <span>
+                        <span>
                         <span>
                             <div>{user.name}</div>
                             <div>{user.status}</div>
@@ -39,10 +41,11 @@ const Users = (props) => {
                             <div>Russia</div>
                         </span>
                     </span>
-                </div> )
-            }
-        </div>
-    )
+                    </div> )
+                }
+            </div>
+        )
+    }
 }
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,5 +1,5 @@
 import {connect} from "react-redux";
-import Users from "./UsersC";
+import Users from "./Users";
 import {followAC, setUserAC, unfollowAC} from "../../redux/users-reducer";
 
 const mapStateToPropsUsers = (state) => {
