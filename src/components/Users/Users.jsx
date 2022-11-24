de5ab12b12e416d75b83dd5da4b5fc96f4b60a36
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-24 20:39:25 +0300

    getting users via click in functional component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,16 +4,19 @@ import userPhoto from "../../assets/images/defaultUser.jpg";
 
 const Users = (props) => {
 
-    if(props.users.length === 0) {
+    let getUsers = () => {
+        if(props.users.length === 0) {
 
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then( response => {
-            props.setUsers(response.data.items);
-        });
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then( response => {
+                props.setUsers(response.data.items);
+            });
 
+        }
     }
 
     return (
         <div>
+            <button onClick={getUsers}>GET USERS</button>
             {
                 props.users.map( user => <div key={user.id}>
                     <span>
