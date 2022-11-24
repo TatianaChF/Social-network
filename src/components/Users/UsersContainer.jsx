6cf5d7a074ad0b5faa838d220121478639a13768
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-24 20:56:19 +0300

    class component import

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,5 +1,5 @@
 import {connect} from "react-redux";
-import Users from "./Users";
+import Users from "./UsersC";
 import {followAC, setUserAC, unfollowAC} from "../../redux/users-reducer";
 
 const mapStateToPropsUsers = (state) => {
