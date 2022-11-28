407674ba2275074183ad3bebf2ca2da3c87655a3
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-28 13:31:02 +0300

    calling a lifecycle method

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,9 +5,7 @@ import userPhoto from "../../assets/images/defaultUser.jpg";
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
-
+    componentDidMount() {
         if (this.props.users.length === 0) {
             axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
                 this.props.setUsers(response.data.items);
