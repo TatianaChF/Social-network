153aeaeb43406cc810316487dcee83b469bd278d
TatyanaChekina <tatian.chefox@gmail.com>
2022-11-29 21:52:39 +0300

    wrapping markup in a presentational component

--- src/components/Users/UsersAPIComponent.jsx
+++ src/components/Users/UsersAPIComponent.jsx
@@ -25,51 +25,6 @@ class UsersAPIComponent extends React.Component {
 
     render() {
 
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-
-        let pages = [];
-
-        for (let i = 1; i <= pagesCount; i++) {
-            if(pages.length < 10) {
-                pages.push(i);
-            }
-        }
-
-        return (
-            <div>
-                <div>
-                    {pages.map( pageNumber => {
-                        return <span className={this.props.currentPage === pageNumber ? styles.pageChoice : ""}
-                        onClick={ () => { this.onPageChanged(pageNumber); } }>{pageNumber}</span>
-                    } )}
-
-                </div>
-                {
-                    this.props.users.map( user => <div key={user.id}>
-                    <span>
-                        <div>
-                            <img src={ user.photos.small != null ? user.photos.small : userPhoto } className={styles.userPhoto} />
-                        </div>
-                        <div>
-                            { user.followed
-                                ? <button onClick={() => { this.props.unfollow(user.id) }}>Unfollow</button>
-                                : <button onClick={() => { this.props.follow(user.id) }}>Follow</button> }
-                        </div>
-                    </span>
-                        <span>
-                        <span>
-                            <div>{user.name}</div>
-                            <div>{user.status}</div>
-                        </span>
-                        <span>
-                            <div>Moscow</div>
-                            <div>Russia</div>
-                        </span>
-                    </span>
-                    </div> )
-                }
-            </div>
-        )
     }
 }
 
