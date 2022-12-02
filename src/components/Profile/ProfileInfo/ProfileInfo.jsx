d230bb9ff0ed5434679499cd467bd7347da09bb6
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-02 18:29:26 +0300

    adding a photo to the page and conditions

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,12 +1,19 @@
 import style from './ProfileInfo.module.css';
+import Preloader from "../../common/Preloader/Preloader";
 
 const ProfileInfo = (props) => {
+
+    if(!props.profile) {
+        return <Preloader />
+    }
+
      return (
          <div>
              <div>
                  <img src="https://oir.mobi/uploads/posts/2021-05/1622211351_27-oir_mobi-p-russkaya-priroda-priroda-krasivo-foto-28.jpg" />
              </div>
              <div className={style.descriptionBlock}>
+                 <img src={props.profile.photos.large}/>
                  ava + description
              </div>
          </div>
