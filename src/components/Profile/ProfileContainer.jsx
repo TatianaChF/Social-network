f0cc854a839292a9bd3b3e834b078499e2c25a5d
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-02 17:47:41 +0300

    adding a get request

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import Profile from "./Profile";
+import axios from "axios";
 
 class ProfileContainer extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/26940`)
+            .then(response => {
+                this.props.setUserProfile(response.data.items);
+            });
+    }
+
     render() {
         return (
             <Profile {...this.props} />
