02ef50f10208b68cccca8c0e76154155423804a8
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-05 14:35:29 +0300

    adding a condition to display the page by default

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -9,6 +9,7 @@ class ProfileContainer extends React.Component {
 
     componentDidMount() {
         let userId = this.props.router.params.userId;
+        if(!userId) userId = 15;
         axios.get(`https://social-network.samuraijs.com/api/1.0/profile/26913` + userId)
             .then(response => {
                 this.props.setUserProfile(response.data);
