75686a1b8a3744a00b631a78644b267a2495cb57
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-05 14:48:42 +0300

    remove default id in request

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -10,7 +10,7 @@ class ProfileContainer extends React.Component {
     componentDidMount() {
         let userId = this.props.router.params.userId;
         if(!userId) userId = 15;
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/26913` + userId)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
             .then(response => {
                 this.props.setUserProfile(response.data);
             });
