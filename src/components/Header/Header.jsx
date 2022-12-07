43073a20b5d32253c8865597d3087d221ab9a580
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-07 16:11:23 +0300

    adding an output condition

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,17 @@
 import style from './Header.module.css';
 import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={style.header}>
             <img src="https://1757140519.rsc.cdn77.org/static/v3/img/products/logo.png" />
 
             <div className={style.loginBlock}>
-                <NavLink to={'/login'}>
-                    Login
-                </NavLink>
+                { props.isAuth ? props.login
+                    : <NavLink to={'/login'}>
+                        Login
+                    </NavLink>}
+
             </div>
         </header>
     )
