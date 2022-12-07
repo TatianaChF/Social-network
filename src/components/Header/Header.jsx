dca9879526725f38840815b59e869011c2ce9f1d
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-07 15:25:55 +0300

    Create a signup/login link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
-import s from './Header.module.css';
+import style from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
     return (
-        <header className={s.header}>
+        <header className={style.header}>
             <img src="https://1757140519.rsc.cdn77.org/static/v3/img/products/logo.png" />
+
+            <div className={style.loginBlock}>
+                <NavLink to={'/login'}>
+                    Login
+                </NavLink>
+            </div>
         </header>
     )
 }
