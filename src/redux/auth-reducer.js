4001dec2c6733c32ce81cbc52ed7c67d898973ea
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-07 16:12:53 +0300

    adding an authorization pointer

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,9 +1,10 @@
-const SET_USER_DATA = 'SET_USER_DATA';
+const SET_USER_DATA = 'SET-USER-DATA';
 
 let initialState = {
     userId: null,
     login: null,
-    email: null
+    email: null,
+    isAuth: false
 };
 
 const authReducer = (state = initialState, action) => {
@@ -12,7 +13,8 @@ const authReducer = (state = initialState, action) => {
         case SET_USER_DATA:
             return {
                 ...state,
-                ...action.data
+                ...action.data,
+                isAuth: true
             }
         default:
             return state;
