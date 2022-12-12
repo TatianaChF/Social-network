eef45eb511b3052cc2d9d0d46dc489baa8a8b7b9
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-12 13:56:11 +0300

    function getUsers call

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -11,16 +11,14 @@ import {
 import axios from "axios";
 import Users from "./Users";
 import Preloader from "../common/Preloader/Preloader";
+import {getUsers} from "../../api/api";
 
 class UsersContainer extends React.Component {
 
     componentDidMount() {
         if (this.props.users.length === 0) {
             this.props.toggleIsFetching(true);
-            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`, {
-                withCredentials: true
-            })
-                .then(response => {
+            getUsers(this.props.currentPage, this.props.pageSize).then(response => {
                     this.props.toggleIsFetching(false);
                     this.props.setUsers(response.data.items);
                     this.props.setTotalUsersCount(response.data.totalCount);
