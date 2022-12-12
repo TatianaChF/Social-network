1cc6a54435d268bb3148aac0b3e6568f04367352
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-12 13:56:42 +0300

    adding default values

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const defaultURL = "https://social-network.samuraijs.com/api/1.0/";
 
-export const getUsers = (currentPage, pageSize) => {
+export const getUsers = (currentPage = 1, pageSize = 10) => {
     return axios.get(defaultURL + `users?page=${currentPage}&count=${pageSize}`, {
         withCredentials: true
     })
