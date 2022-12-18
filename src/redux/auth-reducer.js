8fa1fff5a5f651c1b6d84cc47ba243d1ee585aa0
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-18 12:52:46 +0300

    creating thunk creator

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,3 +1,5 @@
+import {usersAPI} from "../api/api";
+
 const SET_USER_DATA = 'SET-USER-DATA';
 
 let initialState = {
@@ -21,5 +23,18 @@ const authReducer = (state = initialState, action) => {
     }
 }
 
-export const setAuthUserData = (userId, email, login) => ({ type: SET_USER_DATA, data: {userId, email, login} });
+export const setAuthUserData = (userId, email, login) => ({type: SET_USER_DATA, data: {userId, email, login}});
+
+export const getAuth = () => {
+    return (dispatch) => {
+
+        usersAPI.getAuth().then(data => {
+            if (data.resultCode === 0) {
+                let {id, email, login} = data.data;
+                dispatch(setAuthUserData(id, email, login));
+            }
+        });
+    } // thunk
+} //thunk creator
+
 export default authReducer;
