6c5fb06911b14a1b8f2c12c934eee34c867a8aaf
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-18 12:02:27 +0300

    delete this.props

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -73,7 +73,7 @@ export const getUsers = (currentPage, pageSize) => {
     return (dispatch) => {
 
         dispatch(toggleIsFetching(true));
-        usersAPI.getUsers(this.props.currentPage, this.props.pageSize).then(data => {
+        usersAPI.getUsers(currentPage, pageSize).then(data => {
             dispatch(toggleIsFetching(false));
             dispatch(setUsers(data.items));
             dispatch(setTotalUsersCount(data.totalCount));
