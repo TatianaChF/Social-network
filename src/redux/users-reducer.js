df4b7c5d826d1fe319e29decdc0cc2e2a32ed288
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-18 11:37:58 +0300

    export getUsers

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -69,7 +69,7 @@ export const setTotalUsersCount = (totalUsersCount) => ({type: SET_TOTAL_USERS_C
 export const toggleIsFetching = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching});
 export const toggleIsLockedButtons = (isFetching, userId) => ({type: TOGGLE_IS_LOCKED_BUTTONS, isFetching, userId});
 
-const getUsers = (currentPage, pageSize) => {
+export const getUsers = (currentPage, pageSize) => {
     return (dispatch) => {
 
         dispatch(toggleIsFetching(true));
