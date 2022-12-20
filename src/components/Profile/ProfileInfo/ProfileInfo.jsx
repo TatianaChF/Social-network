628b603a818ada91492b141572ded65900688433
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-20 09:03:22 +0300

    creating a default avatar output condition

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import style from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 import greenCircle from "../../../assets/images/green.png";
 import redCircle from "../../../assets/images/red.png";
+import defaultAvatar from "../../../assets/images/defaultUser.jpg";
 
 const ProfileInfo = (props) => {
 
@@ -15,7 +16,7 @@ const ProfileInfo = (props) => {
                  <img src="https://oir.mobi/uploads/posts/2021-05/1622211351_27-oir_mobi-p-russkaya-priroda-priroda-krasivo-foto-28.jpg" />
              </div>
              <div className={style.descriptionBlock}>
-                 <img src={props.profile.photos.large}  alt="avatar" />
+                     <img src={props.profile.photos.large ? props.profile.photos.large : defaultAvatar}  alt="avatar" />
                  <h2>Информация</h2>
                  <div>
                      <p>Id: {props.profile.userId}</p>
