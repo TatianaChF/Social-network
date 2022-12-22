8b63931729a78f52b794f52f1091d0e6822342f5
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-22 18:32:27 +0300

    create state and output conditions input or value

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
@@ -2,15 +2,20 @@ import React from "react";
 
 class ProfileStatus extends React.Component {
 
+    state = {
+        editMode: false
+    }
+
     render() {
         return (
             <div>
-                <div>
-                    <span>{this.props.status}</span>
-                </div>
-                <div>
-                    <input value={this.props.status}/>
-                </div>
+                {this.state.editMode
+                    ? <div>
+                        <input value={this.props.status}/>
+                    </div>
+                    : <div>
+                        <span>{this.props.status}</span>
+                    </div>}
             </div>
         )
     }
