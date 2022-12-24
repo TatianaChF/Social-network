7167b07f0fa56905272dcaf21fb243228125884b
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-24 21:32:18 +0300

    replacing bind with arrow syntax

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
@@ -7,13 +7,13 @@ class ProfileStatus extends React.Component {
         status: this.props.status
     }
 
-    activateEditMode() {
+    activateEditMode = () => {
         this.setState({
             editMode: true
         })
     }
 
-    deactivateEditMode() {
+    deactivateEditMode = () => {
         this.setState({
             editMode: false
         });
@@ -25,10 +25,10 @@ class ProfileStatus extends React.Component {
             <div>
                 {this.state.editMode
                     ? <div>
-                        <input autoFocus={true} onBlur={ this.deactivateEditMode.bind(this) } value={this.state.status}/>
+                        <input autoFocus={true} onBlur={ this.deactivateEditMode } value={this.state.status}/>
                     </div>
                     : <div>
-                        <span onDoubleClick={ this.activateEditMode.bind(this) }>{this.props.status}</span>
+                        <span onDoubleClick={ this.activateEditMode }>{this.props.status}</span>
                     </div>}
             </div>
         )
