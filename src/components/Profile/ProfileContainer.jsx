eb9b73ee1b4e13ddb2abfdf5ae4fc25fac07b2b8
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-24 21:06:18 +0300

    flip status as props

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -19,7 +19,7 @@ class ProfileContainer extends React.Component {
 
     render() {
         return (
-            <Profile {...this.props} profile={this.props.profile} />
+            <Profile {...this.props} profile={this.props.profile} status={this.props.status} />
         )
     }
 
