88782e7b3e5245e1fce324f512398ac4b814eee7
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-24 20:41:34 +0300

    creating an action creator to get the status

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,14 +1,14 @@
 import {usersAPI} from "../api/api";
-import {followSuccess, toggleIsLockedButtons} from "./users-reducer";
 
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 const SET_USER_PROFILE = 'SET-USER-PROFILE';
+const SET_STATUS = 'SET-STATUS';
 
 let initialState = {
     posts: [
-        { id: 1, message: "Hi, how are you?", likesCount: 5 },
-        { id: 2, message: "It's my first post", likesCount: 15 }
+        {id: 1, message: "Hi, how are you?", likesCount: 5},
+        {id: 2, message: "It's my first post", likesCount: 15}
     ],
     newPostText: "Hello everyone!",
     profile: null
@@ -38,18 +38,24 @@ const profileReducer = (state = initialState, action) => {
                 ...state,
                 profile: action.profile
             }
+        case SET_STATUS:
+            return {
+                ...state,
+                status: action.status
+            }
         default:
             return state;
     }
 
 }
 
-export const addPostActionCreator = () => ({ type: ADD_POST });
+// action creators
+export const addPostActionCreator = () => ({type: ADD_POST});
 
 export const updateNewPostTextActionCreator = (text) =>
-    ({ type: UPDATE_NEW_POST_TEXT, newPost: text });
-
-export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile });
+    ({type: UPDATE_NEW_POST_TEXT, newPost: text});
+export const setUserProfile = (profile) => ({type: SET_USER_PROFILE, profile});
+export const setStatus = (status) => ({type: SET_STATUS, status});
 
 export const getUserProfile = (userId) => {
     return (dispatch) => {
