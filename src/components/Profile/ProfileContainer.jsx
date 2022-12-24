35abac58d7a918d1cec27d58ba61587ca6a66d77
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-24 21:07:04 +0300

    flip updateUserStatus as props

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -19,7 +19,8 @@ class ProfileContainer extends React.Component {
 
     render() {
         return (
-            <Profile {...this.props} profile={this.props.profile} status={this.props.status} />
+            <Profile {...this.props} profile={this.props.profile} status={this.props.status}
+                     updateUserStatus={this.props.updateUserStatus} />
         )
     }
 
