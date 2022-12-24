befcdf53f8fda7c01f0a655a176ae926fc40c22b
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-24 21:23:06 +0300

    passing status from local state

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
@@ -24,7 +24,7 @@ class ProfileStatus extends React.Component {
             <div>
                 {this.state.editMode
                     ? <div>
-                        <input autoFocus={true} onBlur={ this.deactivateEditMode.bind(this) } value={this.props.status}/>
+                        <input autoFocus={true} onBlur={ this.deactivateEditMode.bind(this) } value={this.state.status}/>
                     </div>
                     : <div>
                         <span onDoubleClick={ this.activateEditMode.bind(this) }>{this.props.status}</span>
