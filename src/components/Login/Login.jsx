b0cfad7683f44b87e3bebdd7a24807fb75069403
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-26 15:47:17 +0300

    adding name attribute

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,13 +5,13 @@ const LoginForm = (props) => {
         <div>
             <form>
                 <div>
-                    <Field placeholder={"Login"} component={"input"} />
+                    <Field placeholder={"Login"} name={"login"} component={"input"} />
                 </div>
                 <div>
-                    <Field placeholder={"Password"} component={"input"} />
+                    <Field placeholder={"Password"} name={"password"} component={"input"} />
                 </div>
                 <div>
-                    <Field type={"checkbox"} component={"input"} /> remember me
+                    <Field type={"checkbox"} name={"rememberMe"} component={"input"} /> remember me
                 </div>
                 <div>
                     <button>Login</button>
