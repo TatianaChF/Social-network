27b2c1e5844b375b03ef0a837e9e73b9957ced7f
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-26 14:56:51 +0300

    creating form for login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,6 +2,20 @@ const Login = (props) => {
     return (
         <div>
             <h1>LOGIN</h1>
+            <form>
+                <div>
+                    <input placeholder={"Login"}/>
+                </div>
+                <div>
+                    <input placeholder={"Password"}/>
+                </div>
+                <div>
+                    <input type={"checkbox"} /> remember me
+                </div>
+                <div>
+                    <button>Login</button>
+                </div>
+            </form>
         </div>
     )
 }
