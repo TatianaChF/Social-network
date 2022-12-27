0101b54d91850a440517da16c068eef488acff8f
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-27 12:26:09 +0300

    adding a redux-form

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import style from './MyPosts.module.css';
 import Post from "./Post/Post";
+import {Field, reduxForm} from "redux-form";
 
 const MyPosts = (props) => {
 
-    let postsElement = props.posts.map( post => <Post message={post.message} like_count={post.likesCount} /> )
+    let postsElement = props.posts.map(post => <Post message={post.message} like_count={post.likesCount}/>)
 
     let newPostElement = React.createRef();
 
@@ -18,29 +19,36 @@ const MyPosts = (props) => {
     }
 
     return (
-            <div className={style.postsBlock}>
-                <h3>My posts</h3>
-                <MyPostForm onPostChange={onPostChange} newPostElement={newPostElement} onAddPost={onAddPost} />
-                <div className={style.posts}>
-                    { postsElement }
-                </div>
+        <div className={style.postsBlock}>
+            <h3>My posts</h3>
+            <MyPostReduxForm onPostChange={onPostChange} newPostElement={newPostElement} onAddPost={onAddPost}/>
+            <div className={style.posts}>
+                {postsElement}
             </div>
+        </div>
     )
 }
 
 const MyPostForm = (props) => {
     return (
         <div>
-            <div>
-                        <textarea onChange={ props.onPostChange }
-                                  ref={ props.newPostElement }
-                                  value={props.newPostText}></textarea>
-            </div>
-            <div>
-                <button onClick={ props.onAddPost }>Add post</button>
-            </div>
+            <form>
+                <div>
+                    <Field placeholder={"AddingPost"}
+                           name={"posts"}
+                           component={"textarea"}
+                           onChange={props.onPostChange}
+                           ref={props.newPostElement}
+                           value={props.newPostText}/>
+                </div>
+                <div>
+                    <button onClick={props.onAddPost}>Add post</button>
+                </div>
+            </form>
         </div>
     )
 }
+
+let MyPostReduxForm = reduxForm({form: "posts"})(MyPostForm);
 
 export default MyPosts;
