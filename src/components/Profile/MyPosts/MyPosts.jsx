7bfd469b0972a6c2f63d5e7cbb17aa125ebd4fe4
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-28 22:10:45 +0300

    changing the placeholder and passing the Textarea component

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,7 @@ import style from './MyPosts.module.css';
 import Post from "./Post/Post";
 import {Field, reduxForm} from "redux-form";
 import {required, maxLengthCreator} from "../../../utils/validations";
+import Textarea from "../../common/FormsControl/FormsControl";
 
 let maxLength = maxLengthCreator(10);
 
@@ -30,7 +31,7 @@ const MyPostForm = (props) => {
         <div>
             <form onSubmit={props.handleSubmit}>
                 <div>
-                    <Field placeholder="AddingPost" name="newPostText"  component="textarea"
+                    <Field placeholder="Add text" name="newPostText"  component={Textarea}
                            validate={[required, maxLength]} />
                 </div>
                 <div>
