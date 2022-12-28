5f5a62c867e359779c5e5887b37a7519f778eb40
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-28 23:02:48 +0300

    combining logic into one component

--- src/components/common/FormsControl/FormsControl.js
+++ src/components/common/FormsControl/FormsControl.js
@@ -1,23 +1,20 @@
 import styles from "./FormsControl.module.css";
-export const Textarea = ({input, meta, ...props}) => {
+const FormsControl = ({input, meta, child, ...props}) => {
     let hasError = meta.touched && meta.error;
     return (
         <div>
             <div className={styles.formsControl + " " + (hasError ? styles.error : "")} >
-                <textarea {...props} {...input} />
+                {props.child}
             </div>
             {hasError && <span>{meta.error}</span>}
         </div>
     )
 }
+export const Textarea = ({input, meta, ...props}) => {
+
+    return <FormsControl><textarea {...props} {...input} /></FormsControl>
+
+}
 export const Input = ({input, meta, ...props}) => {
-    let hasError = meta.touched && meta.error;
-    return (
-        <div>
-            <div className={styles.formsControl + " " + (hasError ? styles.error : "")} >
-                <input {...props} {...input} />
-            </div>
-            {hasError && <span>{meta.error}</span>}
-        </div>
-    )
+    return <FormsControl><input {...props} {...input} /></FormsControl>
 }
