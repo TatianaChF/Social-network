3d40680bfc0f3641d85f5d84aefb7300fd2d75a9
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-28 23:25:42 +0300

    passing props through a constant

--- src/components/common/FormsControl/FormsControl.js
+++ src/components/common/FormsControl/FormsControl.js
@@ -4,15 +4,17 @@ const FormsControl = ({input, meta, child, ...props}) => {
     return (
         <div>
             <div className={styles.formsControl + " " + (hasError ? styles.error : "")} >
-                <div>{props.child}</div>
+                <div>{props.children}</div>
             </div>
             {hasError && <span>{meta.error}</span>}
         </div>
     )
 }
-export const Textarea = ({input, meta, ...props}) => {
-    return <FormsControl {...props}><textarea {...props} {...input} /></FormsControl>
+export const Textarea = (props) => {
+    const {input, meta, ...restProps} = props;
+    return <FormsControl {...props}><textarea {...restProps} {...input} /></FormsControl>
 }
-export const Input = ({input, meta, ...props}) => {
-    return <FormsControl {...props}><input {...props} {...input} /></FormsControl>
+export const Input = (props) => {
+    const {input, meta, ...restProps} = props;
+    return <FormsControl {...props}><input {...restProps} {...input} /></FormsControl>
 }
