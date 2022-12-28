049cb25a84e250136f82e4bfce9b8d81f57a5070
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-28 23:07:51 +0300

    props transfer

--- src/components/common/FormsControl/FormsControl.js
+++ src/components/common/FormsControl/FormsControl.js
@@ -4,17 +4,15 @@ const FormsControl = ({input, meta, child, ...props}) => {
     return (
         <div>
             <div className={styles.formsControl + " " + (hasError ? styles.error : "")} >
-                {props.child}
+                <div>{props.child}</div>
             </div>
             {hasError && <span>{meta.error}</span>}
         </div>
     )
 }
 export const Textarea = ({input, meta, ...props}) => {
-
-    return <FormsControl><textarea {...props} {...input} /></FormsControl>
-
+    return <FormsControl {...props}><textarea {...props} {...input} /></FormsControl>
 }
 export const Input = ({input, meta, ...props}) => {
-    return <FormsControl><input {...props} {...input} /></FormsControl>
+    return <FormsControl {...props}><input {...props} {...input} /></FormsControl>
 }
