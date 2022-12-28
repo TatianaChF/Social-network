cc40c0163a8b7370e41a8833b27d668dc5dcb47b
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-28 18:02:14 +0300

    creating a maximum length validator

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -2,3 +2,7 @@ export const required = value => {
     if (value) return undefined;
     else return "Field is required!";
 }
+export const maxLength30 = value => {
+    if (value <= 30) return undefined;
+    else return "The length of the post should not be more than 30 characters!";
+}
