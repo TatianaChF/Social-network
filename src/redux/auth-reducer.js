ab761dca7b270b5e285b2466093c524e68ac8155
TatyanaChekina <tatian.chefox@gmail.com>
2022-12-29 11:10:35 +0300

    remove extra isAuth

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -18,8 +18,7 @@ const authReducer = (state = initialState, action) => {
         case SET_USER_DATA:
             return {
                 ...state,
-                ...action.data,
-                isAuth: true
+                ...action.data
             }
         default:
             return state;
